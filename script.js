b97160efa19236e5192d651fac8df58d2c4c8aeb
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-01-23 10:36:35 +0100

    Gamecontroller
    Added Gamecontroller to manage the active players and to add new marks to the board. For this added a new function to the gameboard for placing marks

--- script.js
+++ script.js
@@ -5,36 +5,50 @@ function Gameboard() {
 	const board = []
 
 	// create array of Cell objects
-	for (let x = 0; x < rows; x++) {
-		board[x] = []
-		for(let y = 0; y < columns; y++) {
-			board[x].push(Cell())
+	for (let x = 0;x < rows;x++) {
+		board[ x ] = []
+		for (let y = 0;y < columns;y++) {
+			board[ x ].push(Cell())
 		}
 	}
-	
+
 	const getBoard = () => board
 
+	// output whole board to console
 	const outputBoard = () => {
 		const printedBoard = board.map((row) => row.map((cell) => cell.getValue()))
-		console.log(printedBoard);
+		console.log(printedBoard)
+	}
+
+	const addMarker = (row, column, playerMarker) => {
+		const chosenCell = board[ row ][ column ]
+
+		if (chosenCell.getValue() === "") {
+			chosenCell.setValue(playerMarker)
+			return true
+		} else {
+			return false
+		}
+
 	}
 
 	return {
 		getBoard,
-		outputBoard
+		outputBoard,
+		addMarker
 	}
 }
 
 function Cell() {
-	let value = " "
-
-	// get value of cell
-	const getValue = () => value 
+	let value = ""
 
 	// set value of cell
 	const setValue = (player) => {
 		value = player
 	}
+
+	// get value of cell
+	const getValue = () => value
 
 	return {
 		getValue,
@@ -42,5 +56,43 @@ function Cell() {
 	}
 }
 
-Gameboard().outputBoard()
+const GameController = (function (player1, player2) {
+	// create player objects Array
+	const players = [
+		{
+			name: player1,
+			marker: "X"
+		},
+		{
+			name: player2,
+			marker: "O"
+		}
+	]
+
+	const board = Gameboard()
+
+	let activePlayer = players[ 0 ]
+
+	// swap players
+	const switchActivePlayer = () => {
+		activePlayer = activePlayer === players[ 0 ] ? players[ 1 ] : players[ 0 ]
+	}
+
+	const getActivePlayer = () => activePlayer
+
+	// play round and switch active player if move was available
+	const playRound = (row, column) => {
+		if (board.addMarker(row, column, activePlayer.marker)) {
+			switchActivePlayer()
+		}
+
+
+
+		board.outputBoard()
+	}
+	return {
+		getActivePlayer,
+		playRound
+	}
+})("Player 1", "Player 2")
 
